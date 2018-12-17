b49283c78445c68ffb473d613cce818b3cc517ae
plapicola <p.lapicola@gmail.com>
2018-12-17 09:37:57 -0700

    Adds initital tests for stat_tracker and stat_tracker.rb

---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+class StatTrackerTest < Minitest::Test
+
+  def setup
+    @stat_tracker = StatTracker.new
+  end
+
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+end
