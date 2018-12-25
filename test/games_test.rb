50bf4e25d73a727be31fe2bd0b0d32356ed3ea94
plapicola <p.lapicola@gmail.com>
2018-12-25 11:24:47 -0700

    Adds tests for ability to find all wins and losses for team

--- test/games_test.rb
+++ test/games_test.rb
@@ -6,12 +6,22 @@ class GamesTest < Minitest::Test
     @attributes = { game_id: "2012030221",
                     season: "20122013",
                     type: "P",
-                    date_time: 2013-05-16,
+                    date_time: "2013-05-16",
                     away_team_id: "3",
                     home_team_id: "6",
                     away_goals: 2,
                     home_goals: 3,
                     outcome: "home win OT"
+  }
+  @attributes_2 = { game_id: "2012030222",
+                  season: "20122013",
+                  type: "P",
+                  date_time: "2013-05-17",
+                  away_team_id: "3",
+                  home_team_id: "6",
+                  away_goals: 4,
+                  home_goals: 5,
+                  outcome: "home win OT"
   }
     @games = Games.new
   end
@@ -27,7 +37,7 @@ class GamesTest < Minitest::Test
   end
 
   def test_it_can_create_a_new_game
-    game = @games.create(@attributes)
+    @games.create(@attributes)
 
     assert_equal 2012030221, @games.all.first.game_id
     assert_equal 2, @games.all.first.away_goals
@@ -35,24 +45,14 @@ class GamesTest < Minitest::Test
   end
 
   def test_it_can_search_for_games_by_id
-    game = @games.create(@attributes)
+    @games.create(@attributes)
 
-    assert_equal game.first, @games.find_by_id(2012030221)
+    assert_equal "home win OT", @games.find_by_id(2012030221).outcome
   end
 
   def test_it_can_return_all_games_in_a_season
-    attributes_2 = { game_id: "2012030222",
-                    season: "20122013",
-                    type: "P",
-                    date_time: "2013-05-16",
-                    away_team_id: "3",
-                    home_team_id: "6",
-                    away_goals: 2,
-                    home_goals: 3,
-                    outcome: "home win OT"
-    }
     @games.create(@attributes)
-    @games.create(attributes_2)
+    @games.create(@attributes_2)
 
 
     assert_equal 2, @games.all.count
@@ -63,21 +63,27 @@ class GamesTest < Minitest::Test
   end
 
   def test_it_can_find_games_by_season_id
-    attributes_2 = { game_id: "2012030222",
-                    season: "20122013",
-                    type: "P",
-                    date_time: "2013-05-16",
-                    away_team_id: "3",
-                    home_team_id: "6",
-                    away_goals: 2,
-                    home_goals: 3,
-                    outcome: "home win OT"
-    }
     @games.create(@attributes)
-    @games.create(attributes_2)
+    @games.create(@attributes_2)
 
     assert_equal 2, @games.find_by_season_id(20122013).count
     assert_equal 2012030221, @games.find_by_season_id(20122013).first.game_id
     assert_equal 2012030222, @games.find_by_season_id(20122013).last.game_id
+  end
+
+  def test_it_can_find_all_wins_for_team
+    @games.create(@attributes)
+    @games.create(@attributes_2)
+
+    assert_equal 2, @games.find_wins_by_team(6).count
+    assert_equal [], @games.find_wins_by_team(3)
+  end
+
+  def test_it_can_find_all_losses_for_team
+    @games.create(@attributes)
+    @games.create(@attributes_2)
+
+    assert_equal 2, @games.find_losses_by_team(3).count
+    assert_equal [], @games.find_losses_by_team(6)
   end
 end
