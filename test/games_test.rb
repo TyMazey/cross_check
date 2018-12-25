e3f479a1569b4c582b76aace33349bb643b552f2
plapicola <p.lapicola@gmail.com>
2018-12-25 11:15:52 -0700

    Adds tests for ability to locate games by season id

--- test/games_test.rb
+++ test/games_test.rb
@@ -42,9 +42,9 @@ class GamesTest < Minitest::Test
 
   def test_it_can_return_all_games_in_a_season
     attributes_2 = { game_id: "2012030222",
-                    season: "20122014",
+                    season: "20122013",
                     type: "P",
-                    date_time: 2013-05-16,
+                    date_time: "2013-05-16",
                     away_team_id: "3",
                     home_team_id: "6",
                     away_goals: 2,
@@ -59,5 +59,25 @@ class GamesTest < Minitest::Test
     assert_equal 2012030221, @games.all.first.game_id
     assert_equal 2012030222, @games.all.last.game_id
     assert_equal "home win OT", @games.all.last.outcome
+    @games.all.each {|game| assert_instance_of Game, game}
+  end
+
+  def test_it_can_find_games_by_season_id
+    attributes_2 = { game_id: "2012030222",
+                    season: "20122013",
+                    type: "P",
+                    date_time: "2013-05-16",
+                    away_team_id: "3",
+                    home_team_id: "6",
+                    away_goals: 2,
+                    home_goals: 3,
+                    outcome: "home win OT"
+    }
+    @games.create(@attributes)
+    @games.create(attributes_2)
+
+    assert_equal 2, @games.find_by_season_id(20122013).count
+    assert_equal 2012030221, @games.find_by_season_id(20122013).first.game_id
+    assert_equal 2012030222, @games.find_by_season_id(20122013).last.game_id
   end
 end
