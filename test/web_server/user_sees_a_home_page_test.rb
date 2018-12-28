ad0cef60378d9953d4f8372f67798f425a9f83b9
Ty Mazey <tymazey@gmail.com>
2018-12-27 22:44:54 -0700

    adds 404 page not found test

--- test/web_server/user_sees_a_home_page_test.rb
+++ test/web_server/user_sees_a_home_page_test.rb
@@ -7,4 +7,11 @@ class HomePageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_it_has_error_page_for_unknown_urls
+    visit '/not_found'
+
+    assert page.has_content("Page Not Found")
+    assert_equal 404, page.status_code
+  end
 end
