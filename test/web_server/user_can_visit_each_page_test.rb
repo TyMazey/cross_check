6a088842c037d3b9e9d9f6f7defddc3fedd548cf
Ty Mazey <tymazey@gmail.com>
2018-12-28 11:46:48 -0700

    finish test for each page

--- test/web_server/user_can_visit_each_page_test.rb
+++ test/web_server/user_can_visit_each_page_test.rb
@@ -5,20 +5,35 @@ class HomePageTest < CapybaraTestCase
     visit '/'
     click_on 'Game Statistics'
 
+    assert_equal 200, page.status_code
+    assert_equal '/game_stats', current_path
+    assert page.has_content?("Game Statistics")
   end
 
   def test_user_can_nav_to_league_stats_page
     visit '/'
     click_on 'League Statistics'
+
+    assert_equal 200, page.status_code
+    assert_equal '/league_stats', current_path
+    assert page.has_content?("League Statistics")
   end
 
   def test_user_can_nav_to_season_stats_page
     visit '/'
     click_on 'Season Statistics'
+
+    assert_equal 200, page.status_code
+    assert_equal '/season_stats', current_path
+    assert page.has_content?("Season Statistics")
   end
 
   def test_user_can_nav_to_team_stats_page
     visit '/'
     click_on 'Team Statistics'
+
+    assert_equal 200, page.status_code
+    assert_equal '/team_stats', current_path
+    assert page.has_content?("Team Statistics")
   end
 end
